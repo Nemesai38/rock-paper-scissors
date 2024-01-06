4af18d9213301dd94ddbd0885f40515a8aafc705
Nemesai38 <pappysouls@gmail.com>
2024-01-06 09:20:27 +0100

    UI Finished

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let playerScore = 0;
 let computerScore = 0;
 
+
 // Function to retrieve computer's random option;
 function getComputersChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -16,52 +17,87 @@ function getComputersChoice() {
     }
 }
 
+/* To Display Result on Screen by editing DOM */
+
+const displayResult = document.querySelector('.scores');
+
+const para = document.createElement('p');
+para.style.margin = '0px';
+para.style.whiteSpace = 'pre-wrap';
+para.textContent = '';
+displayResult.appendChild(para);
+
+const computersplay = document.querySelector('#last');
+const compschoice = document.createElement('p')
+compschoice.style.textAlign = 'center';
+compschoice.style.whiteSpace = 'pre-wrap';
+compschoice.style.fontSize = '2rem';
+compschoice.textContecnt = '';
+computersplay.appendChild(compschoice);
+
+const playersplay = document.querySelector('#first');
+const plyrschoice = document.createElement('p')
+plyrschoice.style.textAlign = 'center';
+plyrschoice.style.whiteSpace = 'pre-wrap';
+plyrschoice.style.fontSize = '2rem';
+plyrschoice.textContecnt = '';
+playersplay.appendChild(plyrschoice);
 
 // Function to playout one round of computer's random option and players option
-// , retrieved through prompt command as declared in Ln 23 below, print a WIN, LOSE
-// or TIE option in console and lastly update the respective scores accordingly;
-function playRound() {
-    const playerschoice = prompt("ROCK, PAPER OR SCISSORS?   PICK ONE! : ");
+// , print a WIN, LOSE or TIE option, update the respective scores accordingly;
+// and lastly states winner on the first five wins.
+function playRound(playerschoice) {
     const computerschoice = getComputersChoice();
-    if (playerschoice.toUpperCase() === 'ROCK' && computerschoice === 'PAPER' ||
-        playerschoice.toUpperCase() === 'PAPER' && computerschoice === 'SCISSORS' ||
-        playerschoice.toUpperCase() === 'SCISSORS' && computerschoice === 'ROCK') { 
-            console.log("You Lose!!"), computerScore += 1;
+    if (playerschoice === 'ROCK' && computerschoice === 'PAPER' ||
+        playerschoice === 'PAPER' && computerschoice === 'SCISSORS' ||
+        playerschoice === 'SCISSORS' && computerschoice === 'ROCK') { 
+            computerScore += 1, para.textContent = 'You Lose', 
+            compschoice.textContent = 'Computer played \n' + computerschoice,
+            plyrschoice.textContent = 'You played \n' + playerschoice;
     }
-    if (playerschoice.toUpperCase() === 'PAPER' && computerschoice === 'ROCK' ||
-        playerschoice.toUpperCase() === 'SCISSORS' && computerschoice === 'PAPER' ||
-        playerschoice.toUpperCase() === 'ROCK' && computerschoice === 'SCISSORS') { 
-            console.log("You Win"), playerScore += 1;
+    if (playerschoice === 'PAPER' && computerschoice === 'ROCK' ||
+        playerschoice === 'SCISSORS' && computerschoice === 'PAPER' ||
+        playerschoice === 'ROCK' && computerschoice === 'SCISSORS') { 
+            playerScore += 1, para.textContent = 'You Win';
+            compschoice.textContent = 'Computer played \n' + computerschoice,
+            plyrschoice.textContent = 'You played \n' + playerschoice;
     } 
-    if (playerschoice.toUpperCase() === 'PAPER' && computerschoice === 'PAPER' ||
-        playerschoice.toUpperCase() === 'SCISSORS' && computerschoice === 'SCISSORS' ||
-        playerschoice.toUpperCase() === 'ROCK' && computerschoice === 'ROCK') { 
-            console.log("TIE, Try Again!!");
+    if (playerschoice === 'PAPER' && computerschoice === 'PAPER' ||
+        playerschoice === 'SCISSORS' && computerschoice === 'SCISSORS' ||
+        playerschoice === 'ROCK' && computerschoice === 'ROCK') { 
+            para.textContent = "IT'S A TIE,\nTry Again!!";
+            compschoice.textContent = 'Computer played \n' + computerschoice,
+            plyrschoice.textContent = 'You played \n' + playerschoice;
     }
+    console.log(computerschoice);
+    const writtenScores = document.querySelector('#scores');
+    writtenScores.textContent = playerScore + ' - ' + computerScore;
+
+   /* Function to detect winner */ 
+    function winner() {
+        if (playerScore === 5) {
+            para.textContent = 'CONGRATULATIONS\nWay to Crush it!!\nYou Won the Game'
+        } else if (computerScore === 5) {
+            para.textContent = 'Comp dominated your ass!!\nBETTER LUCK NEXT TIME'
+        }
+    }
+
+    if (playerScore === 5 || computerScore === 5) {    
+        winner();
+    }
+    
 }
 
-// Function to play the above playRound() function 5 times and print updated scores 
-// in console and will lastly check for winner after the 5 rounds and call the function
-// , winner below;
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-        console.log('Your score:', {playerScore})
-        console.log('Computer score:', computerScore);
-    }
-    winner();
-}
+/* Buttons to Select Players Choice */
 
-// Winner function will be called by playGame function after the 5 rounds, compare and 
-// print designated message for the winner in console;
-function winner() {
-    if (computerScore > playerScore) {
-        console.log("\nThe computer dominated your ass! Better luck next time!!")
-    } else if (computerScore < playerScore) {
-        console.log("\nWay to crush it! You win!")
-    } else {
-        console.log("\nHoly shizzers! It's a tie!")
-    }
-}
+const rockbtn = document.querySelector('#rock');
+rockbtn.addEventListener('click', () => {
+    return playRound('ROCK')});
 
-playGame();
+const paperbtn = document.querySelector('#paper');
+paperbtn.addEventListener('click', () => {
+    return playRound('PAPER')});
+
+const scissorsbtn = document.querySelector('#scissors');
+scissorsbtn.addEventListener('click', () => {
+    return playRound('SCISSORS')});
